5fdf0feee0c53629f51b5f8d40f42ef03fcdc881
mohammadhoseein <mohammadhoseeinnemati1384@gmail.com>
2023-11-02 06:21:37 -0700

    first commit

---
+++ fun.py
@@ -0,0 +1,12 @@
+a = int(input("plz enter your farsi score: "))
+b = int(input("piz enter your english score: "))
+c = int(input("plz enter your economy score: "))
+d = int(input("plz enter your chologypsy score: "))
+e = int(input("plz enter your accounting score: "))
+f = int(input("plz enter your math score: "))
+g = int(input("plz enter your ethics score: "))
+
+def my_function1(farsi,english,economy,chologypsy,accounting,math,ethics,):
+    print(farsi + english + economy + chologypsy + accounting + math + ethics / 7)
+
+my_function1(a,b,c,d,e,f,g,)
