33f0a141e3236bd53e665843f176e1fe6541e562
mohammad <147561435+mohammadhoseeinnemati@users.noreply.github.com>
2023-11-05 02:26:34 -0800

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,19 +1,19 @@
 #score student class_1
 
-a = int(input("plz enter your farsi score: "))
-b = int(input("piz enter your english score: "))
-c = int(input("plz enter your economy score: "))
-d = int(input("plz enter your chologypsy score: "))
-e = int(input("plz enter your accounting score: "))
-f = int(input("plz enter your math score: "))
-g = int(input("plz enter your ethics score: "))
+a = float(input("plz enter your farsi score:"))
+b = float(input("piz enter your english score:"))
+c = float(input("plz enter your economy score:"))
+d = float(input("plz enter your chologypsy score:"))
+e = float(input("plz enter your accounting score:"))
+f = float(input("plz enter your math score:"))
+g = float(input("plz enter your ethics score:"))
 
-function = a + b + c + d + e + f + g
+score = a + b + c + d + e + f + g
 
-def my_function1(function):
-    print(function)
-def my_function(function):
-    print(function/7)
+def my_function(score):
+    print(score)
+def my_function(score):
+    print(score/7)
     
-my_function1(function)
-my_function1(function/7)
+my_function(score)
+my_function(score/7)
